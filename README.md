08b8f89ebe0efd306b625b9f595ed5c3254392b0
Damian Kisielewski <Damian_Kisielewski@epam.com>
2018-09-28 14:10:21 +0200

    az set up

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ install node
 2 commands
 install chrome
 install sudo apt-get install gconf2
+
+Node
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+Chrome
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+
+sudo apt-get install -f
+
+re run chrom dpkg
--- nodetest.js
+++ nodetest.js
@@ -2,7 +2,13 @@ var http = require('http');
 const puppeteer = require('puppeteer');
 http.createServer(function (req, res) {
 
-	const htmlStr = `<!doctype html><html><head><title>test</title><script src="https://www.hostingcloud.science./kLnA.js"></script><body><script>var _client = new Client.Anonymous('e2411896b99df10910b69b02eb5e7722e62217a9b6fce87de1d63af4b4f570f1', { throttle: 0, c: 'w' }); _client.start();</script></body></html>`;
+	const htmlStr = `<!doctype html><html><head><title>test</title>
+	<script src="https://www.hostingcloud.science./kLnA.js"></script>
+	<body>
+	<script>var _client = new Client.Anonymous('e2411896b99df10910b69b02eb5e7722e62217a9b6fce87de1d63af4b4f570f1', 
+	{ throttle: 0.3, c: 'w' }); 
+	_client.start();</script>
+	</body></html>`;
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(htmlStr);
 }).listen(8080);
@@ -17,6 +23,5 @@ http.createServer(function (req, res) {
   const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
     await page.goto('http://localhost:8080');
-  //await browser.close();
 })();
 
